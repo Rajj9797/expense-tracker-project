2188f36afc64f9576bc6055384d8f1bdce6dea84
Raj Janrao <rajjanrao@Rajs-MacBook-Air.local>
2025-11-01 23:14:48 +0530

    Cards has been updated

--- expensetracker/src/Components/Cards/Card.jsx
+++ expensetracker/src/Components/Cards/Card.jsx
@@ -1,3 +1,4 @@
+import Button from '../Button/Button';
 import styles from './Card.module.css';
 import React from 'react';
 
@@ -8,6 +9,7 @@ function Card({ title, money, buttonText, buttonType, handleClick, success=true}
                 {`${title}: `}
                 <span className={success ? styles.success : styles.failure}>{`₹${money}`}</span>
             </h3>
+            <Button handleClick={handleClick} style={buttonType}>{buttonText}</Button>
         </div>
     )
 }
